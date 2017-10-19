173de5fd4fbca7b2ecf75b402f575eaccbdaa0b0
Leo Jacobs <leo@the-earthworks.com>
2017-10-19 19:09:02 +0100

    Profil Details Update WIP

--- scripts/app.min.js
+++ scripts/app.min.js
@@ -1687,7 +1687,12 @@ var _ProfileDetailsUpdate = (function (window) {
     $upperCase: $('.new-password-item').find('.uppercase'),
     $number: $('.new-password-item').find('.number'),
     $special: $('.new-password-item').find('.special'),
-    $strength: $('.new-password-item').find('.strength')
+    $strength: $('.new-password-item').find('.strength'),
+
+    /* Endpoints to update data */
+    /* Currently hard Coded to be 0 but when we get the login API we can tie this to the user profile */
+    $nurseEndPoint: 'http://soa.cerdelga.tew-staging.com/api/Nurse/0',
+    $treatmentCentreEndPoint: 'http://soa.cerdelga.tew-staging.com/api/TreatmentCentre/0', /* 0 is hard coded this should be tied up with the nurse data api  */
   };
 
   var bindUIActions = function () {
@@ -1716,8 +1721,20 @@ var _ProfileDetailsUpdate = (function (window) {
       _resetField('.password-edit-block');
     });
 
-  };
+    //Posting the Name Data
+    $('#nurse-name-form').unbind('.submit').on('submit', function (e) {
+      e.preventDefault();
+      _updateFullName();
+    });
 
+
+
+
+
+  }; //End of bindUIActions
+
+
+  //METHODS 
   var _editFullName = function ($clicked) {
     $clicked.parent().hide();
     $('.name-edit-block').fadeIn();
@@ -1729,7 +1746,6 @@ var _ProfileDetailsUpdate = (function (window) {
     $('.password-edit-block').fadeIn();
   };
 
-
   var _resetField = function ($fieldToReset) {
     var el = $($fieldToReset).find('input').not('input[type="submit"]');
     $(el).val('');
@@ -1739,10 +1755,36 @@ var _ProfileDetailsUpdate = (function (window) {
     $('.password-edit-block').find('.new-password-validation').removeClass('showMe');
   };
 
+  var _updateFullName = function(){
+    var newNameValue = $('#newName').val();
+    
+    var formData = {
+      "FullName": newNameValue,
+      "Email": $('#email').val(),
+      "Password": $('#password').val()
+    };
 
+    TEWLibrary.fetchData(_Settings.nurseEndPoint, 'PUT', { $data: formData }).done(_updateFullNameSuccess).fail(_updateNameFail);
+  };
+ 
+  var _updateFullNameSuccess = function(data){
+    $('.name-edit-block').hide();
+    $('#newName').val('');
+    //Updating the exisiting input field
+    $('#fullName').val(data.FullName);
+    $('#nurse-name-form').find('.form-field').css('display', 'block');
 
+    // Just for Display Purpose 
+    var fullName = data.FullName;
+    var firstName = FullName.substring(0, FullName.indexOf(' '));
 
+    $('.nurse-title').text(fullName);
+    $('.link-profile').text(firstName);
+  };
 
+  var _updateNameFail = function(xhr){
+    console.info(xhr.status);
+  };
 
   return {
     init: bindUIActions
