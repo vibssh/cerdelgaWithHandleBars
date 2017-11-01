b15ec71d450b5177674ff48bc76c88b656c6258c
Leo Jacobs <leo@the-earthworks.com>
2017-11-01 16:27:46 +0000

    * Adds scrollTO window on template loader so that the window is taken to top because of hashlink

--- App/js/modules/_templateLoader.js
+++ App/js/modules/_templateLoader.js
@@ -6,6 +6,11 @@ var _TemplateLoader = (function (window) {
    var Template = Cerdelga.templates[$tplName]($data);
    $('#content').html(Template);
    location.hash = tplName;
+
+   /* To stop get the window to the top when template is changed because of hashlink */
+   setTimeout(function() {    
+        window.scrollTo(0, 0);
+      }, 1);
   };
 
   return {
