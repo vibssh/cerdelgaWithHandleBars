411452c87550a75c1a79b8f0db38fdd7e5781977
Leo Jacobs <vibs.sh@gmail.com>
2017-11-02 11:01:13 +0000

    * Fix the Jump of a page on smaller laptop screen due to hash location url

--- App/js/modules/_templateLoader.js
+++ App/js/modules/_templateLoader.js
@@ -1,18 +1,17 @@
 var _TemplateLoader = (function (window) {
   'use strict';
   var init = function (tplName, data) {
-   var $tplName = tplName || location.hash.split('#')[1] || undefined;
-   var $data = data || undefined;
-   var Template = Cerdelga.templates[$tplName]($data);
-   $('#content').html(Template);
-   location.hash = tplName;
+    var $tplName = tplName || location.hash.split('#')[1] || undefined;
+    var $data = data || undefined;
+    var Template = Cerdelga.templates[$tplName]($data);
+    $('#content').html(Template);
+    location.hash = tplName;
 
-   /* To stop get the window to the top when template is changed because of hashlink */
-   setTimeout(function() {    
-        window.scrollTo(0, 0);
-      }, 1);
+    /* To stop get the window to the top when template is changed because of hashlink */
+    setTimeout(function () {
+      window.scrollTo(0, 0);
+    }, 1);
   };
-
   return {
     init: init
   }
